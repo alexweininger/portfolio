fdc4db963bd7fd3bd41df56cc8ba5f976fe8552c
alexweininger <alex.weininger@live.com>
2018-03-17 20:33:47 -0700

    added the beginning of a dynamic sidebar

--- js/script.js
+++ js/script.js
@@ -1,3 +1,26 @@
-document.getElementById('scroll').addEventListener('click', function(){
-    window.scrollBy(0, 100);
-});
+// var blocks = document.getElementsByClassName('content-block');
+// var sidebar = document.getElementById('sidebar');
+// var bars = []
+// var blocksY = [];
+// for (block of blocks) {
+//     var bar = document.createElement('div');
+//     bar.className = 'bar';
+//     sidebar.appendChild(bar);
+//     bars.push(bar);
+//     var blockRect = block.getBoundingClientRect();
+//     blocksY.push(blockRect.top);
+// }
+
+// console.log(blocksY);
+
+// while (true) {
+//     for (i = 0; i < blocksY.length; i++) {
+//         if (window.scrollY >= blocksY[i]){
+//             for(bar of bars){
+//                 bar.classList.remove('active');
+//             }
+//             bars[i].classList.add('active');
+//             //console.log(i);
+//         }
+//     }
+// }
