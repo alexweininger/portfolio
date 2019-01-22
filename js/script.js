96e5aedd0837687a6f5629db622ba97369c4bee5
alexweininger <alex.weininger@live.com>
2019-01-21 18:39:00 -0800

    dynamic updating year

--- js/script.js
+++ js/script.js
@@ -28,3 +28,6 @@
 var parent = document.html;
 var child = document.body;
 child.style.right = child.clientWidth - child.offsetWidth + "px";
+
+let copyrightString = document.getElementById('copyright');
+copyrightString.innerHTML = '©Alex Weininger 2017-' + (new Date).getFullYear();
