fe190a0919cd521bbbc789599a6a503fb183967c
alexweininger <alex.weininger@live.com>
2020-02-13 11:42:16 -0800

    remove text, add github graph

--- js/script.js
+++ js/script.js
@@ -31,3 +31,53 @@ child.style.right = child.clientWidth - child.offsetWidth + "px";
 
 let copyrightString = document.getElementById('copyright');
 copyrightString.innerHTML = 'Alex Weininger 2017-' + (new Date).getFullYear();
+
+(async () => {
+    const GITHUB_USERNAME = 'byliuyang';
+    const COMMITS_CONTAINER = '#github-contributions';
+    const LANGUAGES_CONTAINER = '#github-language-distribution';
+
+    const githubStats = await GithubStats('alexweininger');
+
+    let githubCommits = document.querySelector('#commits');
+    /* Render SVG for commit contributions */
+    let commitsContribSVG = githubStats.commitsContribSVG({
+        rows: 7,
+        space: 4,
+        rectWidth: 16,
+        levelColors: [
+            {
+                minCommits: 0,
+                color: '#ebedf0'
+            },
+            {
+                minCommits: 1,
+                color: '#c6e48b'
+            },
+            {
+                minCommits: 9,
+                color: '#7bc96f'
+            },
+            {
+                minCommits: 17,
+                color: '#239a3b'
+            },
+            {
+                minCommits: 26,
+                color: '#196127'
+            }
+        ]
+    });
+    githubCommits.appendChild(commitsContribSVG);
+
+    let githubLanguageDistribution = document.querySelector(LANGUAGES_CONTAINER);
+    /* Render SVG for language contributions */
+    let languageContribSVG = githubStats.languagesContribSVG({
+        barHeight: 20,
+        barWidth: githubLanguageDistribution.offsetWidth,
+        lineSpacing: 4,
+        languageNameWidth: 100,
+        fontSize: 14
+    });
+    githubLanguageDistribution.appendChild(languageContribSVG);
+})();
